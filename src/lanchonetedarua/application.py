1cbaa018d3c580224b00a2ae0abdb18981381c87
Renan Silva <renanh2008@hotmail.com>
2023-06-20 17:53:53 -0300

    cadastro produto

--- src/lanchonetedarua/application.py
+++ src/lanchonetedarua/application.py
@@ -11,10 +11,12 @@ from web.controllers.produtos.produtos_controller import produtos_controller
 from web.controllers.categorias.categorias_controller import categorias_controller
 
 from domain.services.cliente_service import ClienteService
+from domain.services.produto_service import ProdutoService
 
 def configure_inject() -> None:
     def config(binder: inject.Binder) -> None:
         binder.bind(ClienteService, ClienteService)
+        binder.bind(ProdutoService, ProdutoService)
     inject.configure(config)
 
 def register_routers(app):
@@ -23,6 +25,7 @@ def register_routers(app):
     app.register_blueprint(cliente_controller, url_prefix='/clientes')
     app.register_blueprint(produtos_controller, url_prefix='/produtos')
     app.register_blueprint(categorias_controller, url_prefix='/categorias')
+    
 
 def create_app():
     app = Flask(__name__)
--- src/lanchonetedarua/web/controllers/produtos/produtos_controller.py
+++ src/lanchonetedarua/web/controllers/produtos/produtos_controller.py
@@ -1,11 +1,29 @@
-from flask import Blueprint
+import inject
+from flask import Blueprint, request, jsonify
+from dataclasses import asdict
+
+from domain.services.produto_service import ProdutoService
 
 produtos_controller = Blueprint('produtos_controller', __name__)
 
-@produtos_controller.route('/')
-def get():
-    return "obter produtos"
+@produtos_controller.route('/', methods=['GET'])
+@inject.autoparams()
+def obter_produtos(produto_service: ProdutoService):
+    produtos = produto_service.obter_produtos()
+    return jsonify(produtos)
 
-@produtos_controller.route('/')
-def post():
-    return "obter produtos"
+@produtos_controller.route('/', methods=['POST'])
+def criar_produtos():
+    return jsonify({'mensagem': 'produto criado com sucesso'})
+
+@produtos_controller.route('/<int:produto_id>', methods=['GET'])
+def obter_produto(produto_id):
+    return "produto por ID"
+
+@produtos_controller.route('/<int:produto_id>', methods=['PUT'])
+def atualizar_produto(produto_id):
+    return jsonify({'mensagem': 'produto atualizado com sucesso'})
+
+@produtos_controller.route('/<int:produto_id>', methods=['DELETE'])
+def deletar_produto(produto_id):
+    return jsonify({'mensagem': 'produto deletado com sucesso'})
