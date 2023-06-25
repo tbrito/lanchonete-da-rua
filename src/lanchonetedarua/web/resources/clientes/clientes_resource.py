24e6bc9375ab8605ba5bb9280e84b445f87910a8
Janduy Monroe <janduymonroe@gmail.com>
2023-06-25 19:44:05 -0300

    feat: busca de cliente por cpf

--- src/lanchonetedarua/web/resources/clientes/clientes_resource.py
+++ src/lanchonetedarua/web/resources/clientes/clientes_resource.py
@@ -4,6 +4,7 @@ from flask import request
 
 from domain.entities.cliente import Cliente
 from domain.services.cliente_service import ClienteService
+from web.response_handle.response_handler import ResponseHandler
 from web.resources.clientes.cliente_input import ClienteInput
 
 from container_di import ContainerDI
@@ -17,33 +18,62 @@ class Clientes(Resource):
     def get(self, cliente_id):
          cliente_service = ContainerDI.get(ClienteService)
          cliente = cliente_service.obter_cliente_por_id(cliente_id)
-         return jsonify(cliente)
-
+         return ResponseHandler.success(cliente)
+     
     @api.doc('atualiza um cliente por id')
     @api.expect(_cliente, validate=True)
     def put(self, cliente_id):
         cliente_service = ContainerDI.get(ClienteService)
+        
+        dados_cliente = cliente_service.obter_cliente_por_id(cliente_id)
+        
+        if dados_cliente is None:
+            return ResponseHandler.error('CLiente não existe')
+        
         cliente_data = api.payload
         cliente = ClienteInput(**cliente_data)
         cliente_service.atualizar_cliente(cliente_id, cliente)
         
-        return jsonify({'mensagem': 'Cliente atualizado com sucesso'})
+        return ResponseHandler.success(message='Cliente atualizado com sucesso')
 
     @api.doc('excluir um cliente por id')
     def delete(self, cliente_id):
         cliente_service = ContainerDI.get(ClienteService)
+        
+        cliente = cliente_service.obter_cliente_por_id(cliente_id)
+        
+        if cliente is None:
+            return ResponseHandler.error('Cliente não existe')
+        
         cliente_service.deletar_cliente(cliente_id)
-        return jsonify({'mensagem': 'Cliente deletado com sucesso'})
+        return ResponseHandler.success(data=cliente ,message='Cliente deletado com sucesso')
+    
+    
+@api.route('/cpf/<string:cpf>')
+class ClienteByCPF(Resource):
+   
+    @api.doc('obter um cliente por cpf')
+    def get(self, cpf):
+         cliente_service = ContainerDI.get(ClienteService)
+         cliente = cliente_service.obter_cliente_por_cpf(cpf)
+         
+         if cliente is None:
+             return ResponseHandler.error('Cliente não cadastrado', 404)
+         
+         return ResponseHandler.success(cliente)
+
 
 @api.route('/')
 class ClientesNoParameters(Resource):
     
+    @api.doc('Obter todos os clientes')
     def get(self):
         cliente_service = ContainerDI.get(ClienteService)
         clientes = cliente_service.obter_clientes()
         
-        return jsonify(clientes)
-
+        return ResponseHandler.success(clientes)
+    
+    @api.doc('Criar um cliente')
     @api.expect(_cliente, validate=True)
     def post(self):
         cliente_service = ContainerDI.get(ClienteService)
@@ -51,4 +81,4 @@ class ClientesNoParameters(Resource):
         cliente = ClienteInput(**cliente_data)
         
         cliente_service.criar_cliente(cliente)
-        return jsonify({'mensagem': 'Cliente criado com sucesso'})
+        return ResponseHandler.success(message='Cliente criado com sucesso')
