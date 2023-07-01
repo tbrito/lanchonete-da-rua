7e6ab7a7525772a14b009cdca107f4ae66321831
Janduy Monroe <janduymonroe@gmail.com>
2023-06-30 21:48:23 -0300

    fix: ajustado retorno enum

--- src/lanchonetedarua/domain/entities/pedido.py
+++ src/lanchonetedarua/domain/entities/pedido.py
@@ -9,12 +9,13 @@ class Pedido(Entity):
     cliente_id: int
     session_id: int
     observacoes: str
+    status: str
     def __init__(self, id, cliente_id, session_id, observacoes, status, created_at):
         super().__init__(id, created_at)
         self.cliente_id = cliente_id
         self.session_id = session_id
         self.observacoes = observacoes
-        self.status = status
+        self.status = status.name
 
     def altera_status(self, status):
         self.status = status
